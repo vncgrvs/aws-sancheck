e722b4301a31fd28a63345ff0b2c28f41175e482
Vince <vincent.groves@leanix.net>
2021-03-29 09:59:33 +0200

    v1 setup.py

---
+++ ci_scan/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    with open("requirements.txt", "r") as req:
+        content = req.read()
+        requirements = content.split("\n")
+
+    return requirements
+
+
+setup(
+    name="haws",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points="""
+        [console_scripts]
+        haws=ci_scan.main:cli
+    """,
+)
