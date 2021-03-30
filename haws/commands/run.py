4439cf6abd53618059c5156512f16858b2c39de3
Vince <vincent.groves@leanix.net>
2021-03-31 00:20:47 +0200

    updated v1

--- haws/commands/run.py
+++ haws/commands/run.py
@@ -1,6 +1,8 @@
 import click
 from haws.services.aws.policy_check import *
+from haws.services.aws.organization_check import *
 
 @click.command()
 def cli():
     run_policy_check()
+    run_org_check()
