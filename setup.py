622c1cd102240f82b85f70898e1ccb6046e664a4
Vince <vincent.groves@leanix.net>
2021-03-31 14:42:16 +0200

    updated to v0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_requirements():
 setup(
     name="haws",
     description="A CLI to sanity check the AWS setup for LeanIX Cloud Intelligence Scans",
-    version="0.1",
+    version="0.2.1",
     url="https://github.com/vg-leanix/aws_sancheck",
     author="Vincent Groves",
     author_email="vincent.groves@leanix.net",
